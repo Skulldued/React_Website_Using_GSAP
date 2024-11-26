fdc691e890f9b6f94c9c07b92c5db8db6742bc02
Skulldued <skulldued30@gmail.com>
2024-11-26 22:36:53 +0530

    Without Animation hero section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      sunny
+      <Hero/>
     </main>
   )
 }
