b4f9e3e72797ce9e2ea40aa41058449c710d9f34
Skulldued <skulldued30@gmail.com>
2024-12-02 23:24:15 +0530

    Navbar complteted

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Hero from './components/Hero'
-import About from './components/About'
-
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero/>
-      <About/>
-      
-    </main>
-  )
-}
+    <BrowserRouter>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Navbar />
+        <Hero />
+        <About />
+      </main>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
